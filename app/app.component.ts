5feaa860f3088f1a72382ce057ec382c4fadaf5e
RSkendzic <rskendzic@execom.eu>
2016-08-12 11:47:08 +0200

    retrieving data using http

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core'; 
+import { HTTP_PROVIDERS } from "@angular/http";
+
 import{ ProductListComponent } from './products/product-list.component'; 
+import{ ProductService } from './products/product.service'; 
  
 @Component({
 	// moduleId: module.id,
 	selector: 'pm-app',
 	templateUrl: './app/app.component.html',
-	directives:[ProductListComponent]
+	directives:[ProductListComponent],
+	providers:[ProductService,HTTP_PROVIDERS]
 })
 export class AppComponent implements OnInit {
 	pageTitle: string = "ACME Product Management";
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -20,6 +20,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = "";
+    products: IProduct[];
         
 constructor(private _productService: ProductService)
 {
@@ -30,7 +31,11 @@ toggleImage(): void{
 }
 
 ngOnInit():void{
-    this.products = this._productService.getProducts(); 
+this._productService.getProducts()
+.subscribe(
+    products => this.products = products,
+    error => this.errorMessage = <any>error
+); 
 }
 
 onRatingClicked(message:string):void{
